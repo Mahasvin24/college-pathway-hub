b0642322d3b75563280ed832da96c214f291d591
Mahasvin24 <mahasvin.manikandan@gmail.com>
2024-07-30 21:29:23 -0700

    :Language Done (needs db connect later)

--- app/(dashboard)/language/page.tsx
+++ app/(dashboard)/language/page.tsx
@@ -1,23 +1,57 @@
+"use client"
+
 import {
   Card,
   CardContent,
-  CardDescription,
   CardHeader,
-  CardTitle
+  CardTitle,
+  CardDescription
 } from '@/components/ui/card';
-import { Input } from '@/components/ui/input';
+import { 
+  DropdownMenu, 
+  DropdownMenuContent, 
+  DropdownMenuGroup, 
+  DropdownMenuRadioGroup, 
+  DropdownMenuRadioItem, 
+  DropdownMenuTrigger 
+} from '@/components/ui/dropdown-menu';
+import React from 'react';
 import { Button } from '@/components/ui/button';
-import { Textarea } from '@/components/ui/textarea';
-import { SearchCheck } from 'lucide-react';
 
-export default function LangueagePage() {
-  return(
+export default function LanguagePage() {
+  const [language, setLanguage] = React.useState('English');
+  
+  return (
     <Card>
       <CardHeader>
-        <CardTitle>Language</CardTitle>
-        <CardDescription>Choose language below</CardDescription>
+        <CardTitle>Language / Idioma</CardTitle>
+        <CardDescription>Choose language below / Elija el idioma a continuación</CardDescription>
       </CardHeader>
-      <CardContent></CardContent>
+      <CardContent>
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <Button>{language}</Button>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent>
+            <DropdownMenuGroup>
+              <DropdownMenuRadioGroup>
+                <DropdownMenuRadioItem 
+                  value="english" 
+                  onClick={() => setLanguage("English")}
+                >
+                  English
+                </DropdownMenuRadioItem>
+                <DropdownMenuRadioItem 
+                  value="español" 
+                  onClick={() => setLanguage("Español")}
+                >
+                  Español
+                </DropdownMenuRadioItem>
+              </DropdownMenuRadioGroup>
+            </DropdownMenuGroup>
+          </DropdownMenuContent>
+        </DropdownMenu>
+      </CardContent>
     </Card>
   )
 }
